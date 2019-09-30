12f493832e76cc5f779a970e0fd68320be4892aa
codestealer4546 <mikkonen.reko@gmail.com>
2019-09-30 18:07:37 +0300

    Lisää pythondemo

---
+++ pythondemo.py
@@ -0,0 +1,9 @@
+print("terveisiä! Mikä sinun nimi on?")
+nimi = input()
+print("Moi," + nimi)
+
+if nimi == "Reko":
+    print("Saat 1000000 euroa")
+    print("Kiitos käynnistä")
+else:
+    print("Mene pois")
